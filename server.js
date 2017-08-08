6d2ebf7bdf0c4aef6fbf32d897c15a71953ad6ac
Kartik <kartikgurjar1994@gmail.com>
2017-08-08 22:03:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/counter',function(req,res){
     res.send(counter.toString());
 });
 
-app.get('/ui/main.js', function(){
+app.get('/ui/main.js', function(req,res){
    res.sendFile(path.join(__dirname,'ui','main.js')); 
 });
 
