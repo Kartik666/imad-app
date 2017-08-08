13e23f65969d6c40e3dab7f4da82a9cf033f2819
Kartik <kartikgurjar1994@gmail.com>
2017-08-08 21:58:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,20 @@
-console.log('Loaded!');
+var buttonElement = document.getElementById('button');
+buttonElement.onclick = function(){
+    //create request object
+    var request = new XmlHttpRequest();
+    
+    request.onreadystatechange = function(){
+        if(request.readyState === XmlHttpRequest.DONE){
+            if(request.status === 200){
+               var counterVar = request.responseText;
+               
+               var spanVar = document.getElementById('counter-id');
+               spanVar.innerHtml = counterVar.toString();
+            }
+        }
+    };
+    
+    //make request
+    request.open('GET','http://kartikgurjar1994.imad.hasura-app.io/counter',true);
+    request.send(null);
+};
